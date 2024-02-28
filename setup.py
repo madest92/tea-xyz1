3ffd1f7ef619a80ae30fba0e40f0257532a22903
Klyachin Aleksey <madest92@ozon.ru>
2024-02-27 23:37:49 -0300

    Update info

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import setup
 
 setup(
+    author='Alex',
+    author_email='madest92@mail.com',
     name='tea-xyz1',
-    version='1.0.1',
+    version='1.0.2',
     description='A simple package for https://app.tea.xyz/',
     url='https://github.com/madest92/tea-xyz1',
     project_urls={
         'Homepage': 'https://github.com/madest92/tea-xyz1',
+        'Source': 'https://github.com/madest92/tea-xyz1',
     },
     py_modules=['hello_tea'],
     entry_points={
@@ -22,4 +25,5 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
+    python_requires='>=3.6',
 )
