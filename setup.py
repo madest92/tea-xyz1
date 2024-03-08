023a035c9f3b466f99a4eb3baa8f40707014f7cd
Klyachin Aleksey <madest92@ozon.ru>
2024-03-08 07:33:22 -0300

    update description

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     author='Alex',
     author_email='madest92@mail.com',
     name='tea-xyz1',
-    version='1.0.5',
-    description='A simple package for https://app.tea.xyz/',
+    version='1.0.6',
+    description='A simple package for https://app.tea.xyz/. Example tea-xyz1 - https://github.com/madest92/tea-xyz1 and tea-xyz2 - https://github.com/madest92/tea-xyz2',
     url='https://github.com/madest92/tea-xyz1',
     project_urls={
         'Homepage': 'https://github.com/madest92/tea-xyz1',
